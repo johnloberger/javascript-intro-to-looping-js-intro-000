ab9651bd8674083d03b1e9ea3697a3797747f1c4
John Loberger <johnloberger@gmail.com>
2020-03-24 19:21:44 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -14,3 +14,13 @@ function whileLoop(n){
 	}
 	return "done"
 }
+function doWhileLoop(array){
+	function incrementVariable(){
+		i = i +1
+		return i
+	}
+	do {
+		console.log("I run once regardless.")
+	} while incrementVariable() < 10
+	return Math.random() > 5
+}
